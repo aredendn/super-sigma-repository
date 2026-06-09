4b1790773e8ec1949fa1fe34a8fb6d825b22dd9a
aredendn <ardenpsf@gmail.com>
2026-06-09 22:23:02 +1200

    dont remember

--- cabingame/scripts/cabin.gd
+++ cabingame/scripts/cabin.gd
@@ -13,3 +13,10 @@ func _process(delta: float) -> void:
 
 func _go_to_window_1() -> void:
 	pass # Replace with function body.
+
+
+func _click_area(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	
+	if event is InputEventMouseButton:
+		if str(event.button_index) == "1" and event.pressed:
+			print("enter room")
